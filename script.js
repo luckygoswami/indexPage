6bc590447c7c7c7314a8e042ee07bf5d33e0f0f5
Lucky Goswami <lgoswami204@gmail.com>
2023-11-07 11:24:04 +0530

    feat: added google search bar

---
+++ script.js
@@ -0,0 +1,7 @@
+function googleSearch() {
+    var text = document.getElementById("search").value;
+    var cleanQuery = text.replace(" ", "+", text);
+    var url = "http://www.google.com/search?q=" + cleanQuery;
+
+    window.location.href = url;
+}
